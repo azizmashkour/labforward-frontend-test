f2d7625cf48151144d420140598e7af941e3f228
azizmashkour <azizmashkour@gmail.com>
2019-09-22 00:55:21 +0100

    feat: import chartdatas component and pass it props, then load fake datas

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import RawDatas from '../../datas/SimpleDatas';
+import ChartDatas from './ChartDatas';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [state, setState] = useState({ defaultThreshold: 5 });
+
+  const handleChangeThreshold = (e) => {
+    setState({ defaultThreshold: e.target.value});
+  }
+
+  const inputStyles = {
+    borderColor: "hsl(0,0%,80%)",
+    borderRadius: "4px",
+    borderStyle: "solid",
+    borderWidth: "1px",
+    height: "26px",
+    padding: "2px 10px",
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div className="Thresholds__parent">
+          <label>Change the input value to define your <b>Thresholds</b> </label>
+          <input
+            type="number"
+            onChange={e => handleChangeThreshold(e)}
+            min={1}
+            value={state.defaultThreshold}
+            style={inputStyles}
+            className="Thresholds__input"
+          />
+        </div>
+        <ChartDatas threshold={state.defaultThreshold} data={RawDatas.batch1}/>
+      </div>
     </div>
   );
-}
+
+};
 
 export default App;
