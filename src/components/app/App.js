c9ac9feac8d1231fc7afefd06fedc13e77eb2cd3
azizmashkour <azizmashkour@gmail.com>
2019-09-22 20:18:54 +0100

    feat: app setting should have setting panel for language and threshold

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import { withTranslation } from 'react-i18next';
+import Select from 'react-select';
 import RawDatas from '../../datas/SimpleDatas';
 import ChartDatas from '../canvas/ChartDatas';
+import { options } from '../../configs/options';
 import './App.css';
 
-const App = () => {
-  const [state, setState] = useState({ defaultThreshold: 5 });
+const App = ({lang, i18n, t}) => {
+  const [state, setState] = useState({ defaultThreshold: 5, lang: options[0], });
 
   const handleChangeThreshold = (e) => {
     const parsedValue = parseInt(e.target.value, 10);
     setState({ defaultThreshold: parsedValue });
+  };
+
+  const changeLang = (lang) => {
+    const { value } = lang;
+    i18n.changeLanguage(value);
   };
 
   const inputStyles = {
@@ -21,11 +29,28 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <div className="Container">
-        <div className="Thresholds__parent">
-          <label>
-            Change the input value to define your
+    <div className="Container">
+      <div className="App">
+        <h1 className="Lab__title">{t('Sudden peaks detection')}</h1>
+        <div className="md8">
+          <ChartDatas threshold={state.defaultThreshold} data={RawDatas.batch1} t={t} />
+        </div>
+        <div className="md4">
+          <h2 className="Setting__title">{t('Settings')}</h2>
+          <div className="Thresholds__language">
+            <label>{t('Switch language')}</label>
+            <Select
+              defaultValue={options[0]}
+              options={options}
+              value={lang}
+              onChange={changeLang}
+              className="App-Select"
+            />
+          </div>
+          <div className="Thresholds__parent">
+            <label>
+              {t('Change to update threshold')}
+            </label><br/>
             <input
               type="number"
               onChange={(e) => handleChangeThreshold(e)}
@@ -34,12 +59,11 @@ const App = () => {
               style={inputStyles}
               className="Thresholds__input"
             />
-          </label>
+          </div>
         </div>
-        <ChartDatas threshold={state.defaultThreshold} data={RawDatas.batch1} />
       </div>
     </div>
   );
 };
 
-export default App;
+export default withTranslation()(App);
